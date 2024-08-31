adf29dde03809328a77d7957ef2918583b55139e
Muneer Ahmad <shoaib786asme@gmail.com>
2024-08-31 12:30:10 +0530

    first commit for jenkins

---
+++ src/test/java/socail/Dummy.java
@@ -0,0 +1,35 @@
+package socail;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class Dummy {
+	
+	@Test
+	public void test1()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		String title=driver.getTitle();
+		Assert.assertEquals(title, "Facebook – log in or sign up");
+		driver.close();
+	}
+	
+	@Test
+	public void test2()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		String title=driver.getTitle();
+		Assert.assertEquals(title, "Google");
+		driver.close();
+		
+	}
+	
+	
+
+}
